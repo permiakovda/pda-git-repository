a028b872b924766a60823377e5bd57c22351993b
зукьшфлщмвф <permyakov.d.a.92@mail.ru>
2018-06-09 02:33:52 +0400

    при проверске строки добавлен восклецательный знак на всякий случай

--- task1.js
+++ task1.js
@@ -2,7 +2,7 @@ var a = process.argv[2]
 var b = new String();
 var c = new String();
 for (i=0; i<=a.length; i++){
-  if (a.charAt(i) != "\u0020" && a.charAt(i) != "?")
+  if (a.charAt(i) != "\u0020" && a.charAt(i) != "?" && a.charAt(i) != "!")
   b = b+a.charAt(i);
 }
 for (i=b.length; i>=0; i--)
