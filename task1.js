39ae8085b4782b3a362e4b81b7abe74851936487
зукьшфлщмвф <permyakov.d.a.92@mail.ru>
2018-06-09 02:32:41 +0400

    удалены ненужные строки кода

--- task1.js
+++ task1.js
@@ -1,7 +1,6 @@
 var a = process.argv[2]
 var b = new String();
 var c = new String();
-
 for (i=0; i<=a.length; i++){
   if (a.charAt(i) != "\u0020" && a.charAt(i) != "?")
   b = b+a.charAt(i);
@@ -10,15 +9,6 @@ for (i=b.length; i>=0; i--)
 {
   c = c + b.charAt(i);
 }
-
-
-b = b.toUpperCase();
-c = c.toUpperCase();
-
-console.log(b)
-console.log(c)
-console.log(b.localeCompare(c))
-
 if (b.localeCompare(c) === 0)
 {
   console.log("YES")
@@ -26,5 +16,3 @@ if (b.localeCompare(c) === 0)
 else {
   console.log("NO")
 }
-
-console.log(b.localeCompare(c))
