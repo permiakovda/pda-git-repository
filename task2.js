81f359f71b580b8b2caaa565712e441307b3b2da
зукьшфлщмвф <permyakov.d.a.92@mail.ru>
2018-06-09 22:28:43 +0400

    Теперь по месяцу мужно определить его номер

--- task2.js
+++ task2.js
@@ -2,5 +2,12 @@ var d = Number(process.argv[2]);
 var m = process.argv[3];
 var y = Number(process.argv[4]);
 
+var mnomber = 0;
+
 var monthCount = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
-console.log(newdat);
+
+for (i=0; i<=monthCount.length; i++){
+  if (m.localeCompare(monthCount[i]) === 0){mnomber = i}
+}
+
+console.log(mnomber)
