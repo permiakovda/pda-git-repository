7f1ffd719e66f0507da854693f59c9aac6c066d0
зукьшфлщмвф <permyakov.d.a.92@mail.ru>
2018-06-10 00:51:16 +0400

    пересмотрен алгоритм - решение примера - ок

--- task3.js
+++ task3.js
@@ -1,17 +1,3 @@
-var x1 = Number(process.argv[2]);
-var x2 = Number(process.argv[3]);
-
-
-var date1 = new Date (2011, 0, 1, 0, 0, 0, 0);
-var date2 = new Date (2011, 0, 1, 0, 0, 0, 0);
-
-date1.setSeconds(x1);
-date2.setSeconds(x2);
-
-
-
-var dateSum = new Date;
-
-console.log(date1.getSeconds());
-console.log(date1.getMinutes());
-console.log(date1.getHours());
+var dateSum = new Date (2018, 0, 1, 0, 0, 0, 0);
+dateSum.setSeconds(Number(process.argv[2]) + Number(process.argv[3]))
+console.log(dateSum.getHours() + " Час " + dateSum.getMinutes() + " минут " + dateSum.getSeconds() + " секунд");
