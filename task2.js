f318967d1de752ede160eca0a08ac73600166db1
зукьшфлщмвф <permyakov.d.a.92@mail.ru>
2018-06-09 22:22:22 +0400

    Ввел массив месяцев

---
+++ task2.js
@@ -0,0 +1,6 @@
+var d = Number(process.argv[2]);
+var m = process.argv[3];
+var y = Number(process.argv[4]);
+
+var monthCount = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+console.log(newdat);
