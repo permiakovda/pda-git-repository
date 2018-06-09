17840d91658165ecef43933c633bf08bf9970ab7
зукьшфлщмвф <permyakov.d.a.92@mail.ru>
2018-06-10 01:03:46 +0400

    час с маленькой буквы

--- task3.js
+++ task3.js
@@ -1,3 +1,3 @@
 var dateSum = new Date (2018, 0, 1, 0, 0, 0, 0);
 dateSum.setSeconds(Number(process.argv[2]) + Number(process.argv[3]))
-console.log(dateSum.getHours() + " Час " + dateSum.getMinutes() + " минут " + dateSum.getSeconds() + " секунд");
+console.log(dateSum.getHours() + " час " + dateSum.getMinutes() + " минут " + dateSum.getSeconds() + " секунд");
