9a81634d7dd84610840410f5e79c899a6f911209
зукьшфлщмвф <permyakov.d.a.92@mail.ru>
2018-06-10 15:52:38 +0400

    Задача решщена - надо обрабртать

--- task3.js
+++ task3.js
@@ -1,2 +1,18 @@
 var x1 = Number(process.argv[2]);
 var x2 = Number(process.argv[3]);
+
+var xSum = x1 + x2;
+
+
+var hourCount = Math.abs(Math.round(((xSum/60)/60)-0.5));
+var minetCoun = Math.abs((hourCount*60) - (Math.round((xSum/60)-0.5)));
+var secondCount = Math.abs((hourCount*60*60) + (minetCoun*60) - xSum);
+
+// var minetCount = Math.round((xSum/60)-0.5);
+// var hourCount = Math.round(((xSum/60)/60)-0.5);
+// var secondCount = xSum - hourCount*60*60 - minetCount*60;
+
+
+console.log(hourCount)
+console.log(minetCoun)
+console.log(secondCount)
