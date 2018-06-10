5985e8e05b763f7d4061c173c18e60375640ac23
зукьшфлщмвф <permyakov.d.a.92@mail.ru>
2018-06-10 15:54:15 +0400

    выод результата оформлен, решение примера проходит

--- task3.js
+++ task3.js
@@ -3,16 +3,8 @@ var x2 = Number(process.argv[3]);
 
 var xSum = x1 + x2;
 
-
 var hourCount = Math.abs(Math.round(((xSum/60)/60)-0.5));
 var minetCoun = Math.abs((hourCount*60) - (Math.round((xSum/60)-0.5)));
 var secondCount = Math.abs((hourCount*60*60) + (minetCoun*60) - xSum);
 
-// var minetCount = Math.round((xSum/60)-0.5);
-// var hourCount = Math.round(((xSum/60)/60)-0.5);
-// var secondCount = xSum - hourCount*60*60 - minetCount*60;
-
-
-console.log(hourCount)
-console.log(minetCoun)
-console.log(secondCount)
+console.log(hourCount + " час " + minetCoun + " минут " + secondCount + " секунд")
