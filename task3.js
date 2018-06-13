151bc138ba556cbe1a9b39e2cc8a79a4511b8622
зукьшфлщмвф <permyakov.d.a.92@mail.ru>
2018-06-13 19:18:27 +0400

    Ввел счетчик дней

--- task3.js
+++ task3.js
@@ -3,11 +3,13 @@ var x2 = Number(process.argv[3]);
 
 var xSum = x1 + x2;
 
-var hourCount = Math.abs(Math.round(((xSum/60)/60)-0.5));
-var minetCoun = Math.abs((hourCount*60) - (Math.round((xSum/60)-0.5)));
-var secondCount = Math.abs((hourCount*60*60) + (minetCoun*60) - xSum);
+var daseCount = Math.abs(Math.round((((xSum/60)/60)/24)-0.5));
+var hourCount = Math.abs((Math.round(((xSum/60)/60)-0.5)) - daseCount*24);
+var minetCoun = Math.abs((Math.round(((xSum/60)/60)-0.5))*60 - (Math.round((xSum/60)-0.5)));
+var secondCount = Math.abs(((Math.round(((xSum/60)/60)-0.5))*60*60) + (minetCoun*60) - xSum);
 
-if (hourCount != 0){process.stdout.write(hourCount + " ")}
-if (minetCoun != 0){process.stdout.write(minetCoun + " ")}
-if (secondCount != 0){process.stdout.write(secondCount + " ")}
+if (daseCount != 0){process.stdout.write(daseCount + " дней ")}
+if (hourCount != 0){process.stdout.write(hourCount + " часов ")}
+if (minetCoun != 0){process.stdout.write(minetCoun + " минут ")}
+if (secondCount != 0){process.stdout.write(secondCount + " секунд")}
 console.log("")
