93f2a53e7eff6578e819fa5a238400944feb59fc
зукьшфлщмвф <permyakov.d.a.92@mail.ru>
2018-06-14 19:09:04 +0400

    изменен вывод результатов - теперь смена час часОВ часЫ

--- task3.js
+++ task3.js
@@ -3,13 +3,75 @@ var x2 = Number(process.argv[3]);
 
 var xSum = x1 + x2;
 
-var daseCount = Math.abs(Math.round((((xSum/60)/60)/24)-0.5));
-var hourCount = Math.abs((Math.round(((xSum/60)/60)-0.5)) - daseCount*24);
-var minetCoun = Math.abs((Math.round(((xSum/60)/60)-0.5))*60 - (Math.round((xSum/60)-0.5)));
-var secondCount = Math.abs(((Math.round(((xSum/60)/60)-0.5))*60*60) + (minetCoun*60) - xSum);
+var hourCount = Math.abs(Math.round(((xSum/60)/60)-0.5));
+var minetCoun = Math.abs((hourCount*60) - (Math.round((xSum/60)-0.5)));
+var secondCount = Math.abs((hourCount*60*60) + (minetCoun*60) - xSum);
 
-if (daseCount != 0){process.stdout.write(daseCount + " дней ")}
-if (hourCount != 0){process.stdout.write(hourCount + " часов ")}
-if (minetCoun != 0){process.stdout.write(minetCoun + " минут ")}
-if (secondCount != 0){process.stdout.write(secondCount + " секунд")}
-console.log("")
+var hourName = ""
+var minetName = ""
+var secondName = ""
+
+switch (hourCount%10) {
+  case 1:
+    hourName = " час ";
+    break;
+  case 2:
+  case 3:
+  case 4:
+    hourName = " часа ";
+    break;
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 0:
+    hourName = " часов ";
+    break;
+  default:
+}
+
+switch (minetCoun%10) {
+  case 1:
+    minetName = " минута ";
+    break;
+  case 2:
+  case 3:
+  case 4:
+    minetName = " минуты ";
+    break;
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 0:
+    minetName = " минут ";
+    break;
+  default:
+}
+
+switch (secondCount%10) {
+  case 1:
+    secondName = " секунда ";
+    break;
+  case 2:
+  case 3:
+  case 4:
+    secondName = " секунды ";
+    break;
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 0:
+    secondName = " секунд ";
+    break;
+  default:
+    econdName = " секунд ";
+}
+
+if (hourCount != 0){  process.stdout.write(hourCount + hourName)}
+if (minetCoun != 0){process.stdout.write(minetCoun + minetName)}
+if (secondCount != 0){process.stdout.write(secondCount + secondName)}
