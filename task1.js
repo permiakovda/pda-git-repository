9171d61f432a00741dcaf3ec961ffa06bba588f4
permiakovda <39566422+permiakovda@users.noreply.github.com>
2018-06-15 23:27:36 +0400

    В верхний регистр; длинна -2 символа!

--- task1.js
+++ task1.js
@@ -1,14 +1,19 @@
 var a = process.argv[2]
 var b = new String();
 var c = new String();
-for (i=0; i<=a.length; i++){
-  if (a.charAt(i) != "\u0020" && a.charAt(i) != "?" && a.charAt(i) != "!")
+
+for (i=0; i<=a.length-2; i++){
+  if (a.charAt(i) != "\u0020")
   b = b+a.charAt(i);
 }
 for (i=b.length; i>=0; i--)
 {
   c = c + b.charAt(i);
 }
+
+b = b.toUpperCase();
+c = c.toUpperCase();
+
 if (b.localeCompare(c) === 0)
 {
   console.log("YES")
