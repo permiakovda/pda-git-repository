31c31cd2306bec6b131a57750591b915deea194f
permiakovda <39566422+permiakovda@users.noreply.github.com>
2018-06-16 18:32:18 +0400

    вернул "!" "?" и добавил "."

--- task1.js
+++ task1.js
@@ -2,8 +2,8 @@ var a = process.argv[2]
 var b = new String();
 var c = new String();
 
-for (i=0; i<=a.length-2; i++){
-  if (a.charAt(i) != "\u0020")
+for (i=0; i<=a.length; i++){
+  if (a.charAt(i) != "\u0020" && a.charAt(i) != "?" && a.charAt(i) != "!" && a.charAt(i) != ".")
   b = b+a.charAt(i);
 }
 for (i=b.length; i>=0; i--)
